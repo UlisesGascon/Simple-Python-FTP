4355349c6cd70912b742b0d40e840f807a54e5f0
UlisesGascon <ulisesgascongonzalez@gmail.com>
2015-01-23 15:01:14 +0100

    Config.py was added
    Config.py was added

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+user_ftp = "USER"  # User_ID
+passwrd_ftp = "PASS"  # Password
+path_ftp = "WEB_SITE"  # web_address 
+input_folder = "/home/USER/FTPDown/input"  # Downloaded files from ftp to local machine
+output_folder = "/home/USER/FTPDown/output"  # Stored files to be uploaded into ftp (as default)
